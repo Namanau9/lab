ae399537273a008bc34ded46a790635ecb7302af
Naman A U <119608938+Namanau9@users.noreply.github.com>
2024-12-11 21:20:37 +0530

    Update 9.py

--- Python/9.py
+++ Python/9.py
@@ -1,19 +1,20 @@
-#9.	Write a python program that create a text file write some lines into it, read the contents from the fille and then display it
+#9.	Write a python program that create a text file write some lines into it, 
+#   read the contents from the fille and then display it
 
 def write_to_file(file_name):
     # Open the file in write mode
-    with open(file_name, 'w') as file:
+    file=open(file_name, 'w')
         # Write some lines to the file
-        file.write("Hello, World!\n")
-        file.write("This is a simple file handling example.\n")
-        file.write("Python is great for file operations.\n")
+    file.write("Hello, World!\n")
+    file.write("This is a simple file handling exampllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllle.\n")
+    file.write("Python is great for file operations.\n")
 
 def read_from_file(file_name):
     # Open the file in read mode
-    with open(file_name, 'r') as file:
+    file=open(file_name, 'r')
         # Read the content of the file
-        content = file.read()
-        return content
+    content = file.read()
+    return content
 
 # Main program
 file_name = 'example.txt'
